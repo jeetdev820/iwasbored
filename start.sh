7352f5c561816b2a8f333067d4994aabdea38861
jeet8200 <135669873+jeet8200@users.noreply.github.com>
2025-05-18 06:41:24 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -253,7 +253,7 @@ server {
     return 301 https://\$host\$request_uri;
 }
 EOF
-chmod 755 /etc/nginx
+chmod 750 /etc/nginx
 systemctl reload nginx
   cat > /etc/nginx/stream.d/mtproto.conf <<EOF
 
